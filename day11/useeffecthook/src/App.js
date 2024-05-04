4ed111cc800d0360241a4c3a8a730ffd13eb137a
Sanjay Vikram <cbmsanjay2004@gmail.com>
2024-05-04 21:05:35 +0530

    .

---
+++ day11/useeffecthook/src/App.js
@@ -0,0 +1,14 @@
+import Advice from './Advice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Advice/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
